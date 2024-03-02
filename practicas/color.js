07b0a6866dd74f371a9602873dc87c986b9090ff
leito <ljgeneyl@ufpso.edu.co>
2024-03-01 20:46:20 -0500

    primer commit

---
+++ practicas/color.js
@@ -0,0 +1,9 @@
+function color(color) {
+    document.body.style.backgroundColor = color;
+    document.body.style.color = "white"; // Cambia el color del texto a blanco
+}
+
+function normal(color) {
+    document.body.style.backgroundColor = color;
+    document.body.style.color = "black"; 
+}
